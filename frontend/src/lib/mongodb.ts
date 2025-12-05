fc36ea32b5fcfb23cac5a0859f47115fd39daefc
jenniferyli <jennifer.yuzhen.li@gmail.com>
2025-12-05 12:06:46 -0500

    updated mongodb.ts and scripts

--- frontend/src/lib/mongodb.ts
+++ frontend/src/lib/mongodb.ts
@@ -1,14 +1,16 @@
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGODB_URI as string;
-
-if (!uri) {
-  throw new Error("Please add your Mongo URI to the environment variables");
-}
-
+const uri = process.env.MONGO_URI as string;
 const options = {};
 
+// Validate required env var
+if (!uri) {
+  throw new Error("Please add your Mongo URI to .env.local");
+}
+
+// Extend global type to allow `_mongoClientPromise`
 declare global {
+  // eslint-disable-next-line no-var
   var _mongoClientPromise: Promise<MongoClient> | undefined;
 }
 
@@ -16,17 +18,16 @@ let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
 if (process.env.NODE_ENV === "development") {
+  // Reuse the connection during hot reloads
   if (!global._mongoClientPromise) {
     client = new MongoClient(uri, options);
     global._mongoClientPromise = client.connect();
   }
   clientPromise = global._mongoClientPromise;
 } else {
+  // In production, always create a new client
   client = new MongoClient(uri, options);
   clientPromise = client.connect();
 }
 
-
-export const mongoClientPromise: Promise<MongoClient> = clientPromise;
-
-export default mongoClientPromise;
+export default clientPromise;
