b878ade353411a1a88648f389f62379b7e0134a7
jenniferyli <jennifer.yuzhen.li@gmail.com>
2025-12-05 12:29:40 -0500

    modified local port

--- backend/server.ts
+++ backend/server.ts
@@ -19,11 +19,9 @@ mongoose
 // Routes
 app.use("/api/users", authRoutes);
 
-// Example protected route
-// import { protect } from "./middleware/authMiddleware";
-// app.get("/api/protected", protect, (req, res) => {
-//   res.json({ message: "You are authorized", user: req.user });
-// });
+const PORT = process.env.PORT || 500;
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
